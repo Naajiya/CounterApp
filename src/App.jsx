d0f53beb502b5cb371aa2e0008a3c80c29f860c6
NAJIYA KP <najiyabinthmajeed@gmail.com>
2024-11-26 08:40:53 -0800

    code updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,62 @@
-import { useState } from 'react'
-
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { useDispatch, useSelector } from 'react-redux';
 import { decrement, increament, increamentByAmount, reset } from './Redux/counterSlice';
-
-
+import { Col, Row, Container } from 'react-bootstrap';
 
 function App() {
+  const [amount, setAmount] = useState(0);
+  const { count } = useSelector((state) => state.counterReducer);
+  const dispatch = useDispatch();
 
-  const [amount,setAmout]=useState(0)
-
-  const {count}=useSelector(state=>state.counterReducer)
-  
-  const dispatch=useDispatch()
-
-  const handleincreamentByAmount=()=>{
-    if(amount){
-      dispatch(increamentByAmount(Number(amount)))
-    }else{
-      alert('enter number')
+  const handleIncrementByAmount = () => {
+    if (amount) {
+      dispatch(increamentByAmount(Number(amount)));
+    } else {
+      alert('Enter a number');
     }
-  }
+  };
 
   return (
     <>
-      <div className='container text-light align-items-center justify-content-center text-center'>
-        <h1> Counter App</h1>
-        <div className=' align-items-center justify-content-center text-center border border-info  borde-2 p-5'>
-          <h1 style={{fontSize:'4rem'}}>{count}</h1>
-          <div className='container p-5'>
-            <div className='d-flex justify-content-between'>
-              <Button onClick={()=>dispatch(increament())} variant="outline-warning">Increament</Button>
-              <Button  onClick={()=>dispatch(reset())} variant="outline-danger">Reset</Button>
-              <Button  onClick={()=>dispatch(decrement())} variant="outline-success">Decrement</Button>
-            </div>
-          </div>
-          <div className='d-flex p-3'>
-          <Form.Control type="text" onChange={(e)=>setAmout(e.target.value)} className='m-4' placeholder="increament by amount" />
-            <Button onClick={handleincreamentByAmount} className='m-3' variant="primary">increament by amount</Button>
-          </div>
-        </div>
-      </div>
+      <Container className="text-light text-center mt-5">
+        <h1 className="mb-4">Counter App</h1>
+        <Row className="justify-content-center">
+          <Col xs={12} sm={10} md={8} lg={6} className="border border-info p-4">
+            <h1 className="fs-1">{count}</h1>
+            <Row className="mt-4">
+              <Col className="d-flex justify-content-between">
+                <Button onClick={() => dispatch(increament())} variant="outline-warning">
+                  Increment
+                </Button>
+                <Button onClick={() => dispatch(reset())} variant="outline-danger">
+                  Reset
+                </Button>
+                <Button onClick={() => dispatch(decrement())} variant="outline-success">
+                  Decrement
+                </Button>
+              </Col>
+            </Row>
+            <Row className="mt-4">
+              <Col className="d-flex justify-content-center">
+                <Form.Control
+                  type="number"
+                  onChange={(e) => setAmount(e.target.value)}
+                  className="me-2"
+                  placeholder="Increment by amount"
+                />
+                <Button onClick={handleIncrementByAmount} variant="primary">
+                  Increment by Amount
+                </Button>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
